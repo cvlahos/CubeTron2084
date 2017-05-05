70b7bea962f9260303724a458f8621bcfc3fd134
cvlahos <cvlahos@users.noreply.github.com>
2017-05-05 12:27:15 -0400

    To rotate player

---
+++ TurnPlayer.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnPlayer : MonoBehaviour
+{
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			transform.Rotate(0f,0f,rotateSpeed * Time.deltaTime);	
+		}
+		
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			transform.Rotate(0f,0f,-rotateSpeed * Time.deltaTime);
+			
+		}
+	}
+}
