dda46621c3b972a42eb54f0dd412f76c95f2c13d
cvlahos <cvlahos@users.noreply.github.com>
2017-05-05 13:42:28 -0400

    Snap to Angle

---
+++ TurnPlayerSnap.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurnPlayerSnap : MonoBehaviour
+{
+	public float rotateSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(KeyCode.D)) 
+		{
+			//transform.Rotate(0f,0f,rotateSpeed * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0,0,-90f);
+		}
+		
+		if (Input.GetKey(KeyCode.A)) 
+		{
+			//transform.Rotate(0f,0f,-rotateSpeed * Time.deltaTime);
+			transform.eulerAngles = new Vector3 (0,0,90f);
+			
+		}
+	}
+}
